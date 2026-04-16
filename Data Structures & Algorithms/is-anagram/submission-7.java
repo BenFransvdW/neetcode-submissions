e79df3f53e7f1d7aeb477782834720a3cfab8787
Ben van der Westhuizen <benvdwest7@gmail.com>
2026-04-16 14:01:02 +0200

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> sOccurrences = new HashMap<>();
+        Map<Character, Integer> tOccurrences = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (sOccurrences.keySet().contains(s.charAt(i))) {
+                sOccurrences.merge(s.charAt(i), 1, Integer::sum);
+            } else {
+                sOccurrences.put(s.charAt(i), 1);
+            }
+            if (tOccurrences.keySet().contains(t.charAt(i))) {
+                tOccurrences.merge(t.charAt(i), 1, Integer::sum);
+            } else {
+                tOccurrences.put(t.charAt(i), 1);
+            }
+        }
+        return sOccurrences.equals(tOccurrences);
+    }
+}
