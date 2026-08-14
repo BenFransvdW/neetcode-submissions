a6f1200f5bb0d537fac7046211bf04a9f869085e
Ben van der Westhuizen <benvdwest7@gmail.com>
2026-08-14 14:42:09 +0200

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return (new String(sChars)).equals(new String(tChars));
+    }
+}
